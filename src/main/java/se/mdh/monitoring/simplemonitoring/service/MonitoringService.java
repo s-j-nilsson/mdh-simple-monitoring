6f973b9d0f3631e2ca40e16e1ef4a2b510532ee2
Johan Nilsson <s.j.nilsson@gmail.com>
2018-10-04 13:18:05 +0200

    Ny filtreringsmekanism

--- src/main/java/se/mdh/monitoring/simplemonitoring/service/MonitoringService.java
+++ src/main/java/se/mdh/monitoring/simplemonitoring/service/MonitoringService.java
@@ -2,17 +2,15 @@ package se.mdh.monitoring.simplemonitoring.service;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.MissingNode;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.json.JsonParser;
-import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.stereotype.Service;
 import se.mdh.monitoring.simplemonitoring.rest.RestClient;
 
@@ -20,10 +18,9 @@ import se.mdh.monitoring.simplemonitoring.rest.RestClient;
 public class MonitoringService {
   private static final Log log = LogFactory.getLog(MonitoringService.class);
 
-
   private RestClient restClient;
 
-  @Value("${nagios.monitored.services}")
+  @Value("${nagios.monitored.services.filter:}")
   private String[] services;
 
   public MonitoringService(RestClient restClient) {
@@ -33,24 +30,34 @@ public class MonitoringService {
   public Map<String, String> getStatusMap() {
     Map<String, String> statusMap = new TreeMap<>();
     try {
+      List<String> servicesList = Arrays.asList(services);
+
       String nagiosServiceJson = restClient.getNagiosServiceJson();
-      JsonParser jsonParser = JsonParserFactory.getJsonParser();
-      Map<String, Object> jsonMap = jsonParser.parseMap(nagiosServiceJson);
 
       ObjectMapper objectMapper = new ObjectMapper();
       JsonNode jsonRootNode = objectMapper.readTree(nagiosServiceJson);
 
-      List<String> serviceList = Arrays.asList(services);
-      for(String serviceName : serviceList) {
-        List<JsonNode> values = jsonRootNode.findValues(serviceName);
-        for(JsonNode serviceNode : values) {
-          if(!(serviceNode instanceof MissingNode)) {
-            JsonNode serviceDescription = serviceNode.findValue("service_description");
-            JsonNode currentState = serviceNode.findValue("current_state");
-            statusMap.put(serviceDescription.textValue() + (values.size() > 1 ? " (" + serviceNode.findValue("host_name").textValue()  + ")" : ""), currentState.textValue());
-          }
-          else {
-            log.warn("Kunde inte hitta tjänsten med namn " + serviceName);
+      JsonNode servicesRootNode = jsonRootNode.get("services");
+
+      Iterator<Map.Entry<String, JsonNode>> hostFields = servicesRootNode.fields();
+      while(hostFields.hasNext()) {
+        Map.Entry<String, JsonNode> hostEntry = hostFields.next();
+        Iterator<Map.Entry<String, JsonNode>> serviceFields = hostEntry.getValue().fields();
+        while(serviceFields.hasNext()) {
+          Map.Entry<String, JsonNode> serviceEntry = serviceFields.next();
+
+          JsonNode serviceDescription = serviceEntry.getValue().findValue("service_description");
+          JsonNode currentState = serviceEntry.getValue().findValue("current_state");
+          List<JsonNode> values = jsonRootNode.findValues(serviceEntry.getKey());
+
+          if(!servicesList.isEmpty()) {
+            for(String service : servicesList) {
+              if(serviceEntry.getKey().toLowerCase().contains(service.toLowerCase())) {
+                statusMap.put(serviceDescription.textValue() + (values.size() > 1 ? " (" + serviceEntry.getValue().findValue("host_name").textValue()  + ")" : ""), currentState.textValue());
+              }
+            }
+          } else {
+            statusMap.put(serviceDescription.textValue() + (values.size() > 1 ? " (" + serviceEntry.getValue().findValue("host_name").textValue()  + ")" : ""), currentState.textValue());
           }
         }
       }
